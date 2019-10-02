1da2da62ee1f99e459b8385e44ecdef6483b5a02
priya8084 <55181539+priya8084@users.noreply.github.com>
2019-10-02 19:08:58 +1000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,29 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="front.css">
+    
+</head>
+<body>
+       <div class="navbar">
+    <a href="front.php"> HOME </a>
+       <div class="dropdown">
+      <button class="dropbtn">CATEGORY<i class="fa fa-caret-down"></i>
+      </button>
+  <div class="dropdown-content">
+  <a href="kids.php">Kids</a>
+  <a href="#">Crime</a>
+  <a href="#">Education</a>
+  <a href="#">Romance</a>
+  </div>
+</div>
+<div class="navbar1">
+    <a href = "login.php">LOGIN</a>
+    </div>
+</div>
+<h1 style="text-align:center">SUPER BOOK STORE</h1>
+<br><br>
+</body>
+</html>
